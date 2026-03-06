ec3ac579077d055a98e923c73dc8871166de31f0
Alex Culango <wizardoncouch@gmail.com>
2026-03-06 09:30:50 +0800

    add hydrate fallback

--- app/root.tsx
+++ app/root.tsx
@@ -45,6 +45,14 @@ export default function App() {
   return <Outlet />;
 }
 
+export function HydrateFallback() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
+    </div>
+  );
+}
+
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let message = "Oops!";
   let details = "An unexpected error occurred.";
